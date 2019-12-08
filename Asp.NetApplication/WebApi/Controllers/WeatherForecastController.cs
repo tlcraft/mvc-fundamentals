320e58105125df8daf3040b93dbb25c07945c094
Travis Craft <tlcraft@users.noreply.github.com>
2019-12-08 12:03:14 -0700

    Add Web Api.

---
+++ Asp.NetApplication/WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> logger;
+        private readonly ICurrentDateService systemDateService;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, ICurrentDateService systemDateService)
+        {
+            this.logger = logger;
+            this.systemDateService = systemDateService;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("currentDate")]
+        public string GetCurrentDate()
+        {
+            return systemDateService.CurrentDate;
+        }
+    }
+}
