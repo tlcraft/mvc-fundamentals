86328a1f303e38b4ed9f3a0e69c373056bc38c9f
Travis Craft <tlcraft@users.noreply.github.com>
2019-12-17 17:29:37 -0700

    Add initial db context.

---
+++ Asp.NetApplication/DAL/EfContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;");
+        }
+    }
+}
