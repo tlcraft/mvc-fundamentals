aa89501cee27f5622ba49622a72174f015f22437
Travis Craft <tlcraft@users.noreply.github.com>
2019-12-25 15:01:44 -0700

    Add UserService singleton to Web API.

--- Asp.NetApplication/WebApi/Startup.cs
+++ Asp.NetApplication/WebApi/Startup.cs
@@ -29,6 +29,7 @@ namespace WebApi
                 options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]);
                 options.UseInternalServiceProvider(serviceProvider);
             });
+            services.AddSingleton<IUserService, UserService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
