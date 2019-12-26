c1f9e5b2db318cccb971ffad3ab3f36bf503daa1
Travis Craft <tlcraft@users.noreply.github.com>
2019-12-26 13:25:37 -0700

    Replace ef context with user service in Razor project.

--- Asp.NetApplication/RazorPagesApp/Pages/Index.cshtml.cs
+++ Asp.NetApplication/RazorPagesApp/Pages/Index.cshtml.cs
@@ -1,5 +1,4 @@
-﻿using DAL;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Shared.Services;
 
 namespace RazorPagesApp.Pages
@@ -7,20 +6,20 @@ namespace RazorPagesApp.Pages
     public class IndexModel : PageModel
     {
         private readonly ICurrentDateService currentDateService;
-        private readonly EfContext efContext;
+        private readonly IUserService userService;
         public string CurrentDate { get; set; }
         public string UserName { get; set; }
 
-        public IndexModel(ICurrentDateService currentDateService, EfContext efContext)
+        public IndexModel(ICurrentDateService currentDateService, IUserService userService)
         {
             this.currentDateService = currentDateService;
-            this.efContext = efContext;
+            this.userService = userService;
         }
 
         public void OnGet()
         {
             this.CurrentDate = currentDateService.CurrentDate;
-            this.UserName = this.efContext.Users.Find(1).FirstName;
+            this.UserName = this.userService.GetUser(1).FirstName;
         }
     }
 }
