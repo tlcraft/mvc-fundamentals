942d03d1ee885a4cd797e8799ca89468802b436d
Travis Craft <tlcraft@users.noreply.github.com>
2019-12-28 10:58:50 -0700

    Add initial native date class.

---
+++ Asp.NetApplication/Shared/Services/NativeCurrentDateService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Shared.Services
+{
+    public class NativeCurrentDateService : ICurrentDateService
+    {
+        public string CurrentDate => throw new NotImplementedException();
+    }
+}
