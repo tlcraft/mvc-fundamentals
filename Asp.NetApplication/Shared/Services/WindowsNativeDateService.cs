47249c630142c582ecfe63daa2da459da577bbf3
Travis Craft <tlcraft@users.noreply.github.com>
2019-12-30 11:54:39 -0700

    Add Windows native date service.

---
+++ Asp.NetApplication/Shared/Services/WindowsNativeDateService.cs
@@ -0,0 +1,21 @@
+﻿using Shared.Models;
+using System.Runtime.InteropServices;
+
+namespace Shared.Services
+{
+    public class WindowsNativeDateService : ICurrentDateService
+    {
+        [DllImport("kernel32.dll")]
+        static extern void GetSystemTime(SystemTime systemTime);
+
+        public string CurrentDate
+        {
+            get
+            {
+                var systemTime = new SystemTime();
+                GetSystemTime(systemTime);
+                return systemTime.ToString();
+            }
+        }
+    }
+}
