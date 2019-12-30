e8858f5304cd4e791b89ab4c6801eebb71ede63a
Travis Craft <tlcraft@users.noreply.github.com>
2019-12-30 11:54:59 -0700

    Add native date factory.

---
+++ Asp.NetApplication/Shared/Services/NativeCurrentDateFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Shared.Services
+{
+    public class NativeCurrentDateFactory : INativeCurrentDateFactory
+    {
+        public ICurrentDateService GetNativeCurrentDateService()
+        {
+            var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+            if(isWindows)
+            {
+                return new WindowsNativeDateService();
+            }
+            else
+            {
+                return new CurrentDateService();
+            }
+        }
+    }
+}
