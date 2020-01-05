9805ce082e7a96db065066af7ab57101bbcca661
Travis Craft <tlcraft@users.noreply.github.com>
2020-01-05 12:06:14 -0700

    Inject user service to home controller.

--- Asp.NetApplication/MvcFrameworkApp/Controllers/HomeController.cs
+++ Asp.NetApplication/MvcFrameworkApp/Controllers/HomeController.cs
@@ -7,16 +7,19 @@ namespace MvcFrameworkApp.Controllers
     public class HomeController : Controller
     {
         private ICurrentDateService currentDateService;
+        private IUserService userService;
 
-        public HomeController(ICurrentDateServiceFactory currentDateServiceFactory)
+        public HomeController(ICurrentDateServiceFactory currentDateServiceFactory, IUserService userService)
         {
             this.currentDateService = currentDateServiceFactory.GetCurrentDateService();
+            this.userService = userService;
         }
 
         public ActionResult Index()
         {
             var model = new IndexModel();
             model.CurrentDate = currentDateService.CurrentDate;
+            model.User = userService.GetUser(1);
             return View(model);
         }
 
