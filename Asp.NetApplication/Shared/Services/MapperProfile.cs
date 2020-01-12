ccc39af2beff8411e97147b599dc77640e2a1b17
Travis Craft <tlcraft@users.noreply.github.com>
2020-01-12 10:52:23 -0700

    Add MapperProfile.

---
+++ Asp.NetApplication/Shared/Services/MapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DAL;
+using Shared.Models;
+
+namespace Shared.Services
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<User, UserModel>();
+        }
+    }
+}
