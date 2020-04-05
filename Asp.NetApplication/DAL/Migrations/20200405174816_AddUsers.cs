1aa5c83d986cbdc012750dd17ec3d266b40cdab5
Travis Craft <tlcraft@users.noreply.github.com>
2020-04-05 12:05:43 -0600

    Add Users migration.

---
+++ Asp.NetApplication/DAL/Migrations/20200405174816_AddUsers.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class AddUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                INSERT INTO dbo.Users (FirstName, LastName) 
+                VALUES 
+                    ('Mario', 'Mario'),
+                    ('Luigi', 'Mario')
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE FROM dbo.Users WHERE FirstName = 'Mario' AND LastName = 'Mario'");
+            migrationBuilder.Sql(@"DELETE FROM dbo.Users WHERE FirstName = 'Luigi' AND LastName = 'Mario'");
+        }
+    }
+}
