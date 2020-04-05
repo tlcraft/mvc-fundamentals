8a0b26e6b9bbc698d9809296d04edcf260cfc875
Travis Craft <tlcraft@users.noreply.github.com>
2020-04-05 12:44:34 -0600

    Add IsSubscribed migration.

---
+++ Asp.NetApplication/DAL/Migrations/20200405184118_AddIsSubscribedToUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class AddIsSubscribedToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSubscribedToNewsletter",
+                table: "Users",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSubscribedToNewsletter",
+                table: "Users");
+        }
+    }
+}
