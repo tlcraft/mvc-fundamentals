311c3944d44ba762ecba81416b0b0f0583dece48
Travis Craft <tlcraft@users.noreply.github.com>
2020-04-05 12:44:24 -0600

    Add IsSubscribed to User.

--- Asp.NetApplication/DAL/User.cs
+++ Asp.NetApplication/DAL/User.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte? MembershipTypeId { get; set; }
     }
