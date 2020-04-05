2d80a403beabd6fd8fa3985e8c197d24cfa1eb22
Travis Craft <tlcraft@users.noreply.github.com>
2020-04-05 12:39:53 -0600

    Add MembershipType to User.

---
+++ Asp.NetApplication/DAL/MembershipType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    public class MembershipType
+    {
+        public byte Id { get; set; }
+        public short SignUpFee { get; set; }
+        public byte DurationInMonths { get; set; }
+        public byte DiscountRate { get; set; }
+    }
+}
