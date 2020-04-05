abc9725b9e23b7daabb85043a1240f4053377443
Travis Craft <tlcraft@users.noreply.github.com>
2020-04-05 12:03:56 -0600

    Add Game Db model.

---
+++ Asp.NetApplication/DAL/Game.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DAL
+{
+    public class Game
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
