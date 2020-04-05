cb54030be5e8fdf56df43a4d05d941eb510a0859
Travis Craft <tlcraft@users.noreply.github.com>
2020-04-04 19:56:28 -0600

    Add Customer and Game shared models.

---
+++ Asp.NetApplication/Shared/Models/Game.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Shared.Models
+{
+    public class Game
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
