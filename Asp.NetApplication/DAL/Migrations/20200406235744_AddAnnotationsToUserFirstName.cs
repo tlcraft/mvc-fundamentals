73190458707e1d5d5378d382cf6ed872ab73defd
Travis Craft <tlcraft@users.noreply.github.com>
2020-04-06 17:59:47 -0600

    Add User FirstName migration.

---
+++ Asp.NetApplication/DAL/Migrations/20200406235744_AddAnnotationsToUserFirstName.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class AddAnnotationsToUserFirstName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255);
+        }
+    }
+}
