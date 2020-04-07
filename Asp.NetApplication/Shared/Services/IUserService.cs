1d99f997c093c2c66c2986723420bbe9926667c7
Travis Craft <tlcraft@users.noreply.github.com>
2020-04-06 21:01:34 -0600

    Add GetAllUsers to UserService.

--- Asp.NetApplication/Shared/Services/IUserService.cs
+++ Asp.NetApplication/Shared/Services/IUserService.cs
@@ -1,9 +1,11 @@
 ﻿using Shared.Models;
+using System.Collections.Generic;
 
 namespace Shared.Services
 {
     public interface IUserService
     {
+        List<UserModel> GetAllUsers();
         UserModel GetUser(int id);
     }
 }
--- Asp.NetApplication/Shared/Services/UserService.cs
+++ Asp.NetApplication/Shared/Services/UserService.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using DAL;
 using Shared.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Shared.Services
 {
@@ -20,6 +22,13 @@ namespace Shared.Services
             var user = this.efContext.Users.Find(id);
             var userModel = this.mapper.Map<User, UserModel>(user);
             return userModel;
+        }
+
+        public List<UserModel> GetAllUsers()
+        {
+            var users = this.efContext.Users.ToList();
+            var userModels = this.mapper.Map<List<User>, List<UserModel>>(users);
+            return userModels;
         }
     }
 }
