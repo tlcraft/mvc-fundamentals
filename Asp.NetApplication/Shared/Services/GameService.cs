2e652960bdb26e45e8f49210eb7f44ce16b35c90
Travis Craft <tlcraft@users.noreply.github.com>
2020-04-06 20:58:19 -0600

    Add GameService.

---
+++ Asp.NetApplication/Shared/Services/GameService.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using DAL;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shared.Services
+{
+    public class GameService : IGameService
+    {
+        private EfContext efContext;
+        private IMapper mapper;
+
+        public GameService(EfContext efContext, IMapper mapper)
+        {
+            this.efContext = efContext;
+            this.mapper = mapper;
+        }
+
+        public List<Models.Game> GetAllGames()
+        {
+            var games = this.efContext.Games.ToList();
+            var gameModels = this.mapper.Map<List<Game>, List<Models.Game>>(games);
+            return gameModels;
+        }
+    }
+}
