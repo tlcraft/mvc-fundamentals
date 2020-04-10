5ce848d166dc2526ef79f3d076e902164b547318
Travis Craft <tlcraft@users.noreply.github.com>
2020-04-09 20:51:56 -0600

    Add MembershipTypeIdName migration.

---
+++ Asp.NetApplication/DAL/Migrations/20200410020335_AddMembershipTypeIdName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class AddMembershipTypeIdName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "MembershipType",
+                newName: "MembershipTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MembershipTypeId",
+                table: "MembershipType",
+                newName: "Id");
+        }
+    }
+}
