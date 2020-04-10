dd5668f5243cb7dd423d9b0439317b94f185f3ed
Travis Craft <tlcraft@users.noreply.github.com>
2020-04-09 20:50:18 -0600

    Name MembershipTypeId column.

--- Asp.NetApplication/DAL/MembershipType.cs
+++ Asp.NetApplication/DAL/MembershipType.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DAL
 {
     public class MembershipType
     {
+        [Column(nameof(MembershipType) + "Id")]
         public byte Id { get; set; }
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
