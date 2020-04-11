1faa4b013b612ac61cbc121c2b7daf0336c2ebf3
Travis Craft <tlcraft@users.noreply.github.com>
2020-04-11 12:35:29 -0600

    Add GenreType.

---
+++ Asp.NetApplication/DAL/GenreType.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DAL
+{
+    public class GenreType
+    {
+        [Column(nameof(GenreType) + "Id")]
+        public byte Id { get; set; }
+        public string Name { get; set; }
+    }
+}
