872c742d0eda88ba4df8a8394dd61f0200bea6a9
Travis Craft <tlcraft@users.noreply.github.com>
2020-04-10 22:19:34 -0600

    Add UserBirthdate migration.

--- Asp.NetApplication/DAL/Migrations/20200411033418_AddUserBirthdate.Designer.cs
+++ Asp.NetApplication/DAL/Migrations/20200411033418_AddUserBirthdate.Designer.cs
@@ -4,14 +4,16 @@ using DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DAL.Migrations
 {
     [DbContext(typeof(EfContext))]
-    partial class EfContextModelSnapshot : ModelSnapshot
+    [Migration("20200411033418_AddUserBirthdate")]
+    partial class AddUserBirthdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
