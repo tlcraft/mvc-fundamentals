6bc181323906726397986f85055bcaccf9398f24
Travis Craft <tlcraft@users.noreply.github.com>
2020-04-16 20:17:06 -0600

    Add Required annotation to game name.

--- Asp.NetApplication/Shared/Models/GameModel.cs
+++ Asp.NetApplication/Shared/Models/GameModel.cs
@@ -1,11 +1,13 @@
 ﻿using DAL;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Shared.Models
 {
     public class GameModel
     {
         public long Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public DateTime ReleaseDate { get; set; }
         public GenreType GenreType { get; set; }
