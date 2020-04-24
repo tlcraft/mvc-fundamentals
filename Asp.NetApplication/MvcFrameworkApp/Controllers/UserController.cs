4a61e0e0e9326b07e79539465d1ab37c0dc2a64d
Travis Craft <tlcraft@users.noreply.github.com>
2020-04-23 20:36:15 -0600

    Create a UserController.

---
+++ Asp.NetApplication/MvcFrameworkApp/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Shared.Services;
+using System.Web.Mvc;
+
+namespace MvcFrameworkApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private IUserService userService;
+
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public ActionResult GetCustomers()
+        {
+            var customers = this.userService.GetAllUsers();
+            return View("Customers", customers);
+        }
+
+        [Route("User/Customer/{customerId}")]
+        public ActionResult GetCustomer(int customerId)
+        {
+            var selectedCustomer = this.userService.GetUser(customerId);
+            return View("Customer", selectedCustomer);
+        }
+    }
+}
