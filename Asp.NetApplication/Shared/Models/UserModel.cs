3dccf501b7efffb3757c48268c38eba9005e5376
Travis Craft <tlcraft@users.noreply.github.com>
2020-04-24 20:12:12 -0600

    Add UserModel data annotations.

--- Asp.NetApplication/Shared/Models/UserModel.cs
+++ Asp.NetApplication/Shared/Models/UserModel.cs
@@ -1,14 +1,20 @@
 ﻿using DAL;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Shared.Models
 {
     public class UserModel
     {
         public long Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required] 
         public string LastName { get; set; }
+        [Required] 
         public MembershipType MembershipType { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime Birthdate { get; set; }
     }
 }
