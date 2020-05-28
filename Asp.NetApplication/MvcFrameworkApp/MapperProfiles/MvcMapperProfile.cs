e28196333b7a96524f686b92111e34e6e88b2062
Travis Craft <tlcraft@users.noreply.github.com>
2020-05-27 18:46:40 -0600

    Add new mapper profile for the MVC project.

---
+++ Asp.NetApplication/MvcFrameworkApp/MapperProfiles/MvcMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DAL;
+using MvcFrameworkApp.Models;
+using Shared.Models;
+
+namespace MvcFrameworkApp.MapperProfiles
+{
+    public class MvcMapperProfile : Profile
+    {
+        public MvcMapperProfile()
+        {
+            CreateMap<GameFormViewModel, GameModel>()
+                .ForMember(dest => dest.GenreType, opt => opt.MapFrom(src => new GenreType { Id = src.GenreTypeId } ));
+        }
+    }
+}
