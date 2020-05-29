f661cea307b2a0ece2805989ce25c11d122477d9
Travis Craft <tlcraft@users.noreply.github.com>
2020-05-28 18:54:30 -0600

    Refactor view model for game properties.

--- Asp.NetApplication/MvcFrameworkApp/Models/GameFormViewModel.cs
+++ Asp.NetApplication/MvcFrameworkApp/Models/GameFormViewModel.cs
@@ -1,12 +1,23 @@
 ﻿using DAL;
-using Shared.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcFrameworkApp.Models
 {
     public class GameFormViewModel
     {
         public IEnumerable<GenreType> GenreTypes { get; set; }
-        public GameModel GameModel { get; set; }
+        public long? Id { get; set; }
+        [Required]
+        [StringLength(450, ErrorMessage = "Name cannot be longer than 450 characters.")]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        [Display(Name = "Genre")]
+        public byte GenreTypeId { get; set; }
     }
 }
