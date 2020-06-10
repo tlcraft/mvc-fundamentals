4133992366263e917119c2d6daaf4678c99f1cc7
Travis Craft <tlcraft@users.noreply.github.com>
2020-06-09 21:08:26 -0600

    Add initial rental properties.

--- Asp.NetApplication/Shared/Models/RentalModel.cs
+++ Asp.NetApplication/Shared/Models/RentalModel.cs
@@ -1,6 +1,10 @@
-﻿namespace Shared.Models
+﻿using System.Collections.Generic;
+
+namespace Shared.Models
 {
     public class RentalModel
     {
+        public int CustomerId { get; set; }
+        public List<int> MoveieIds { get; set; }
     }
 }
