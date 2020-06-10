6c86f065d017fcaeb3247ef7386dd8fa80594927
Travis Craft <tlcraft@users.noreply.github.com>
2020-06-09 21:06:07 -0600

    Add initial RentalController.

---
+++ Asp.NetApplication/WebApi/Controllers/RentalController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
+using System;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    public class RentalController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateRental(RentalModel rental)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
