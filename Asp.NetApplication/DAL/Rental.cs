11a3b1e1eb8e026e695442d1c8f14e9f9892e893
Travis Craft <tlcraft@users.noreply.github.com>
2020-06-10 17:20:39 -0600

    Add initial Rental model.

---
+++ Asp.NetApplication/DAL/Rental.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DAL
+{
+    public class Rental
+    {
+        [Column(nameof(Rental) + "Id")]
+        public byte Id { get; set; }
+        public DateTime DateRented { get; set; }
+        public DateTime DateReturned { get; set; }
+        public User User { get; set; }
+        public byte UserId { get; set; }
+        public User Game { get; set; }
+        public byte GameId { get; set; }
+    }
+}
