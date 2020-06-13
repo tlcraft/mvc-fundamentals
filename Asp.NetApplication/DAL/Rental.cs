1ed79bc37e79276306547dcaa0723dc24a7d7310
Travis Craft <tlcraft@users.noreply.github.com>
2020-06-12 18:58:24 -0600

    Update rental foreign keys.

--- Asp.NetApplication/DAL/Rental.cs
+++ Asp.NetApplication/DAL/Rental.cs
@@ -12,7 +12,11 @@ namespace DAL
         public DateTime? DateReturned { get; set; }
         [Required]
         public User User { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }
         [Required]
         public Game Game { get; set; }
+        [ForeignKey("Game")]
+        public long GameId { get; set; }
     }
 }
