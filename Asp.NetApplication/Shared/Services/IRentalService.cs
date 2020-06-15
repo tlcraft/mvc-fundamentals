26ae83a8d227a8a487300e098046e6f60ee1d179
Travis Craft <tlcraft@users.noreply.github.com>
2020-06-15 17:18:25 -0600

    Add IRentalService interface.

---
+++ Asp.NetApplication/Shared/Services/IRentalService.cs
@@ -0,0 +1,14 @@
+﻿using Shared.Models;
+using System.Collections.Generic;
+
+namespace Shared.Services
+{
+    public interface IRentalService
+    {
+        List<RentalModel> GetAllRentals();
+        RentalModel GeRentalById(long rentalId);
+        int AddRental(RentalModel newRental);
+        int UpdateRental(RentalModel rentalModel);
+        int DeleteRental(long rentalId);
+    }
+}
