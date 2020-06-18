8ad321761ec496eb750ca11376c554059f3bb2da
Travis Craft <tlcraft@users.noreply.github.com>
2020-06-18 14:25:05 -0600

    Edit IRentalService.

--- Asp.NetApplication/Shared/Services/IRentalService.cs
+++ Asp.NetApplication/Shared/Services/IRentalService.cs
@@ -1,12 +1,11 @@
 ﻿using Shared.Models;
-using System.Collections.Generic;
 
 namespace Shared.Services
 {
     public interface IRentalService
     {
-        List<RentalModel> GetAllRentals();
-        RentalModel GeRentalById(long rentalId);
+        RentalModel GetAllRentalsByUserId(long userId);
+        RentalModel GetAllRentalsByRentalId(long rentalId);
         int AddRental(RentalModel newRental);
         int UpdateRental(RentalModel rentalModel);
         int DeleteRental(long rentalId);
