b5b6af405d276e1a2c3f5d986d25fd8e8ef4c59c
Travis Craft <tlcraft@users.noreply.github.com>
2020-06-23 17:39:07 -0600

    Add GetRentals endpoint.

--- Asp.NetApplication/WebApi/Controllers/RentalsController.cs
+++ Asp.NetApplication/WebApi/Controllers/RentalsController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Shared.Models;
+using Shared.Services;
 using System;
 
 namespace WebApi.Controllers
 {
     [ApiController]
+    [Route("[controller]")]
     public class RentalsController : ControllerBase
     {
+        private IRentalService rentalService;
+
+        public RentalsController(IRentalService rentalService)
+        {
+            this.rentalService = rentalService;
+        }
+
+        [HttpGet]
+        [Route("{userId}")]
+        public RentalModel GetRentals(long userId)
+        {
+            var rentals = this.rentalService.GetAllRentalsByUserId(userId);
+
+            return rentals;
+        }
+
         [HttpPost]
         public IActionResult CreateRental(RentalModel rental)
         {
