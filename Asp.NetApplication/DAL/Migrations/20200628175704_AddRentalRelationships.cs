d9caf910da46611a130041b6f6592765f3cb23ee
Travis Craft <tlcraft@users.noreply.github.com>
2020-06-28 12:28:24 -0600

    Add Rental relationships migration.

---
+++ Asp.NetApplication/DAL/Migrations/20200628175704_AddRentalRelationships.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class AddRentalRelationships : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Rentals_GameId",
+                table: "Rentals");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rentals_UserId",
+                table: "Rentals");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rentals_GameId",
+                table: "Rentals",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rentals_UserId",
+                table: "Rentals",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Rentals_GameId",
+                table: "Rentals");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rentals_UserId",
+                table: "Rentals");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rentals_GameId",
+                table: "Rentals",
+                column: "GameId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rentals_UserId",
+                table: "Rentals",
+                column: "UserId",
+                unique: true);
+        }
+    }
+}
