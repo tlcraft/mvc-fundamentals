5bb84226b3b20e12e8137e99e618c28434fb7b6c
Travis Craft <tlcraft@users.noreply.github.com>
2020-07-25 09:40:27 -0600

    Define initial Edit page form.

--- Asp.NetApplication/RazorPagesApp/Pages/Games/Edit.cshtml.cs
+++ Asp.NetApplication/RazorPagesApp/Pages/Games/Edit.cshtml.cs
@@ -1,23 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Shared.Models;
 using Shared.Services;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RazorPagesApp.Pages.Games
 {
     public class EditModel : PageModel
     {
         private readonly IGameService gameService;
-        public GameModel Game { get; set; }
+        private readonly IReferenceService referenceService;
 
-        public EditModel(IGameService gameService)
+        public GameModel Game { get; set; }
+        public IEnumerable<SelectListItem> Genres { get; set; }
+        public EditModel(IGameService gameService, IReferenceService referenceService)
         {
             this.gameService = gameService;
+            this.referenceService = referenceService;
         }
 
         public IActionResult OnGet(long gameId)
         {
             Game = this.gameService.GetGameById(gameId);
+            Genres = this.referenceService.GetGenreTypes().Select(genre => new SelectListItem()
+            {
+                Selected = Game.GenreType.Id == genre.Id,
+                Text = genre.Name, 
+                Value = genre.Name 
+            });
 
             if (Game == null)
             {
