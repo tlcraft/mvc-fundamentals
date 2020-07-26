80e58d5d753fd4f6a3d3143f18d5d8c3f2ea5097
Travis Craft <tlcraft@users.noreply.github.com>
2020-07-26 15:15:19 -0600

    Add form validation and details redirect.

--- Asp.NetApplication/RazorPagesApp/Pages/Games/Edit.cshtml.cs
+++ Asp.NetApplication/RazorPagesApp/Pages/Games/Edit.cshtml.cs
@@ -13,6 +13,7 @@ namespace RazorPagesApp.Pages.Games
         private readonly IGameService gameService;
         private readonly IReferenceService referenceService;
 
+        [BindProperty]
         public GameModel Game { get; set; }
         public IEnumerable<SelectListItem> Genres { get; set; }
         public EditModel(IGameService gameService, IReferenceService referenceService)
@@ -24,19 +25,37 @@ namespace RazorPagesApp.Pages.Games
         public IActionResult OnGet(long gameId)
         {
             Game = this.gameService.GetGameById(gameId);
-            Genres = this.referenceService.GetGenreTypes().Select(genre => new SelectListItem()
-            {
-                Selected = Game.GenreType.Id == genre.Id,
-                Text = genre.Name, 
-                Value = genre.Name 
-            });
 
             if (Game == null)
             {
                 return RedirectToPage("./NotFound");
             }
 
+            InitializeGenreTypes();
             return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var genreId = this.referenceService.GetGenreTypes().Single(genre => genre.Name == Game.GenreType.Name).Id;
+                Game.GenreType.Id = genreId;
+                gameService.UpdateGame(Game);
+            }
+
+            InitializeGenreTypes();
+            return RedirectToPage("./Detail", new { gameId = Game.Id });
+        }
+
+        private void InitializeGenreTypes()
+        {
+            Genres = this.referenceService.GetGenreTypes().Select(genre => new SelectListItem()
+            {
+                Selected = Game.GenreType.Id == genre.Id,
+                Text = genre.Name,
+                Value = genre.Name
+            });
         }
     }
 }
