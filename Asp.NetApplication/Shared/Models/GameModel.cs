52a4fbbd7b07fb00cd8fa248ca060c06be48082b
Travis Craft <tlcraft@users.noreply.github.com>
2020-08-18 17:15:38 -0600

    Add display attributes for resource file to game model.

--- Asp.NetApplication/Shared/Models/GameModel.cs
+++ Asp.NetApplication/Shared/Models/GameModel.cs
@@ -8,14 +8,16 @@ namespace Shared.Models
     {
         public long Id { get; set; }
         [Required]
+        [Display(Name = "GameName", ResourceType = typeof(StringResource))]
         [StringLength(450, ErrorMessage = "Name cannot be longer than 450 characters.")]
         public string Name { get; set; }
         [Required]
         [DataType(DataType.Date)]
+        [Display(Name = "GameReleaseDate", ResourceType = typeof(StringResource))]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         [Required]
-        [Display(Name = "Genre")]
+        [Display(Name = "GameGenreType", ResourceType =typeof(StringResource))]
         public GenreType GenreType { get; set; }
     }
 }
