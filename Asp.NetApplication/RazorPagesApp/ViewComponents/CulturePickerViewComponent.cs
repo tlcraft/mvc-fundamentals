aa0f8940c50b51cb0bbe9b690103ccca1f0a6b79
Travis Craft <tlcraft@users.noreply.github.com>
2020-09-05 12:48:10 -0600

    Add culture picker view components.

---
+++ Asp.NetApplication/RazorPagesApp/ViewComponents/CulturePickerViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Models;
+using System.Linq;
+
+namespace RazorPagesApp.ViewComponents
+{
+    public class CulturePickerViewComponent : ViewComponent
+    {
+        private readonly IOptions<RequestLocalizationOptions> localizationOptions;
+        public CulturePickerViewComponent(IOptions<RequestLocalizationOptions> localizationOptions)
+        {
+            this.localizationOptions = localizationOptions;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var cultureFeature = HttpContext.Features.Get<IRequestCultureFeature>();
+
+            var model = new CultureManagerModel
+            {
+                SupportedCultures = localizationOptions.Value.SupportedCultures.ToList(),
+                CurrentUICulture = cultureFeature.RequestCulture.UICulture
+            };
+
+            return View(model);
+        }
+    }
+}
