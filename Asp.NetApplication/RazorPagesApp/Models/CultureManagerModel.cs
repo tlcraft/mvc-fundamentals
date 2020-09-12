e3f95090d432979f8bb3dc225ada1b43635ee595
Travis Craft <tlcraft@users.noreply.github.com>
2020-09-12 12:00:37 -0600

    Add list example to documentation.

--- Asp.NetApplication/RazorPagesApp/Models/CultureManagerModel.cs
+++ Asp.NetApplication/RazorPagesApp/Models/CultureManagerModel.cs
@@ -5,6 +5,16 @@ namespace Models
 {
     /// <summary>
     /// The CultureManagerModel is used to determine which user selected language is currently set for localization purposes. It also contains the full set of supported languages.
+    /// <list type="bullet">
+    /// <item>
+    /// <term>CurrentUICulture</term>
+    /// <description>Gets the value of the currently set UI culture.</description>
+    /// </item>
+    /// <item>
+    /// <term>SupportedCultures</term>
+    /// <description>Gets the list of supported UI cultures.</description>
+    /// </item>
+    /// </list>
     /// </summary>
     public class CultureManagerModel
     {
